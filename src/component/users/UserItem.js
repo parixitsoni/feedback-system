529ba3766f1fd98b54747ca7f7d234a6fdb75bff
parixitsoni <parikshiitsoni85@gmail.com>
2019-10-12 21:19:43 +0530

    Change in Navbar

--- src/component/users/UserItem.js
+++ src/component/users/UserItem.js
@@ -5,7 +5,7 @@ const UserItem = ({user: { login, avatar_url, html_url }}) =>  {
         return (
             <div className="card text-center" style={{width:'200px'}}>
                 <img src={(avatar_url)} alt="" className="round-img" style={{width:'5rem'}}/>
-                <h3>{login}</h3>
+           
                 <div>
                     <a href={html_url} className="btn btn-dark btn-sm my-1">About</a>
                 </div>
