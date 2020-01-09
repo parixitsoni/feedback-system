cafafbfd2cd17f4dbbe7644b64a6db6274a8acc8
parixitsoni <parikshitsoni85@gmail.com>
2020-01-10 03:26:19 +0530

    Update PostData.js
    Change baseURL, this is final change i swear.

--- src/services/PostData.js
+++ src/services/PostData.js
@@ -1,5 +1,5 @@
 export function PostData(type, userData) {
-  let BaseURL = "https://remotemysql.com/phpmyadmin/index.php";
+  let BaseURL = "https://remotemysql.com/phpmyadmin/index.php?db=C3SW7sDrmJ";
   return new Promise((resolve, reject) => {
     fetch(BaseURL + "?tp=" + type, {
       method: "POST",
