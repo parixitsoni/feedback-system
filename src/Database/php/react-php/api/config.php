9c7d805cd445a51ccf13f04c201a9ae18582ca38
parixitsoni <parikshitsoni85@gmail.com>
2020-01-10 01:38:53 +0530

    Add connection with live db

---
+++ src/Database/php/react-php/api/config.php
@@ -0,0 +1,10 @@
+<?php
+   $rest_json = file_get_contents("php://input");
+   $_POST = json_decode($rest_json, true);
+     $servername = "remotemysql.com:3306";
+     $username = "C3SW7sDrmJ";
+     $password = "ux72fN5tCu";
+     $dbname = "C3SW7sDrmJ";
+   $db = new mysqli($servername, $username, $password, $dbname);
+if(!$db) die("database connection error");
+?>
