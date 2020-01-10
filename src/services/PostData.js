65188ece880e1ac91ea464653b4b4eb30a9b6f1c
parixitsoni <parikshitsoni85@gmail.com>
2020-01-10 14:07:29 +0530

    Update PostData.js
    edit BaseURL

--- src/services/PostData.js
+++ src/services/PostData.js
@@ -1,5 +1,5 @@
 export function PostData(type, userData) {
-  let BaseURL = "https://remotemysql.com/stats.php?dbid=C3SW7sDrmJ";
+  let BaseURL = "https://github.com/parixitsoni/feedback-system/blob/master/src/Database/php/react-php/api/index.php";
   return new Promise((resolve, reject) => {
     fetch(BaseURL + "?tp=" + type, {
       method: "POST",
