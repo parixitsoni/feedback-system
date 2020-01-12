7fb311cb4c819cfae2b489bdffb7bb8b92bec08f
parixitsoni <parikshitsoni85@gmail.com>
2020-01-12 15:59:32 +0530

    Update index.php
    comment link

--- src/Database/php/react-php/api/index.php
+++ src/Database/php/react-php/api/index.php
@@ -12,14 +12,14 @@ elseif($type=='review2') review2();
 
 function login() 
 { 
-    $rest_json = file_get_contents("php://input");
-    $_POST = json_decode($rest_json, true);
-      $servername = "https://remotemysql.com:3306";
-      $username = "C3SW7sDrmJ";
-      $password = "ux72fN5tCu";
-      $dbname = "C3SW7sDrmJ";
-    $db = new mysqli($servername, $username, $password, $dbname);
- if(!$db) die("database connection error");
+//     $rest_json = file_get_contents("php://input");
+//     $_POST = json_decode($rest_json, true);
+//       $servername = "https://remotemysql.com:3306";
+//       $username = "C3SW7sDrmJ";
+//       $password = "ux72fN5tCu";
+//       $dbname = "C3SW7sDrmJ";
+//     $db = new mysqli($servername, $username, $password, $dbname);
+//  if(!$db) die("database connection error");
 
        $json = json_decode(file_get_contents('php://input'), true); 
        $un = $json['un']; $pwd = $json['pwd']; 
