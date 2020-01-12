2801dafe522f635625eb7732fc1e91547b0a29f6
parixitsoni <parikshitsoni85@gmail.com>
2020-01-12 13:46:32 +0530

    Update PostData.js
    remove temp1 variavble

--- src/services/PostData.js
+++ src/services/PostData.js
@@ -1,9 +1,9 @@
 export function PostData(type, userData) {
-  let BaseURL = "https://github.com/parixitsoni/feedback-system/blob/master/src/Database/php/react-php/api/config.php/";
+  let BaseURL = "https://github.com/parixitsoni/feedback-system/blob/master/src/Database/php/react-php/api/index.php";
   // let temp = "https://github.com/parixitsoni/feedback-system/blob/master/src/Database/php/react-php/api/config.php/";
-  let temp1 = "https://github.com/parixitsoni/feedback-system/blob/master/src/Database/php/react-php/api/index.php";
+  // let temp1 = "https://github.com/parixitsoni/feedback-system/blob/master/src/Database/php/react-php/api/index.php";
   return new Promise((resolve, reject) => {
-    fetch(BaseURL + temp1 + "?tp=" + type, {
+    fetch(BaseURL + "?tp=" + type, {
       method: "POST",
       headers: {
         "Accept": "application/json;",
