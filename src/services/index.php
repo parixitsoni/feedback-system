5a0873dbc8d275c463a432cc04e4fde8f8a68e87
parixitsoni <parikshitsoni85@gmail.com>
2020-01-13 18:24:35 +0530

    Update index.php
    remove comments

--- src/services/index.php
+++ src/services/index.php
@@ -13,15 +13,6 @@ elseif($type=='review2') review2();
 function login() 
 { 
     require 'config.php'; 
-//     $rest_json = file_get_contents("php://input");
-//     $_POST = json_decode($rest_json, true);
-//       $servername = "https://remotemysql.com:3306";
-//       $username = "C3SW7sDrmJ";
-//       $password = "ux72fN5tCu";
-//       $dbname = "C3SW7sDrmJ";
-//     $db = new mysqli($servername, $username, $password, $dbname);
-//  if(!$db) die("database connection error");
-
        $json = json_decode(file_get_contents('php://input'), true); 
        $un = $json['un']; $pwd = $json['pwd']; 
        $userData =''; 
@@ -146,16 +137,6 @@ function rate2(){
 }
 
 function feed(){
-    
-    $rest_json = file_get_contents("php://input");
-    $_POST = json_decode($rest_json, true);
-      $servername = "https://remotemysql.com/phpmyadmin/db_structure.php?server=1&db=C3SW7sDrmJ/";
-      $username = "C3SW7sDrmJ";
-      $password = "ux72fN5tCu";
-      $dbname = "C3SW7sDrmJ";
-    $db = new mysqli($servername, $username, $password, $dbname);
- if(!$db) die("database connection error");
-
     $json = json_decode(file_get_contents('php://input'), true);
     $semester=$_POST['semester'];
     $query = "SELECT * FROM `semester`";
