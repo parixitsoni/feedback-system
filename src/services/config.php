772631234d4ca7777cf594bfbd872698f1adfcfb
parixitsoni <parikshitsoni85@gmail.com>
2020-01-18 11:36:22 +0530

    change url

--- src/services/config.php
+++ src/services/config.php
@@ -1,10 +1,10 @@
 <?php
    $rest_json = file_get_contents("php://input");
    $_POST = json_decode($rest_json, true);
-     $servername = "google.com";
-     $username = "C3SW7sDrmJ";
-     $password = "ux72fN5tCu";
-     $dbname = "C3SW7sDrmJ";
+     $servername = "sql12.freemysqlhosting.net";
+     $username = "sql12319457";
+     $password = "7E9XHyz4N5";
+     $dbname = "sql12319457";
    $db = new mysqli($servername, $username, $password, $dbname);
 if(!$db) die("database connection error");
 ?>
