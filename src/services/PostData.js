e6f5ae6f70e6c1241f537129773232a5b01f411c
parixitsoni <parikshitsoni85@gmail.com>
2020-01-18 11:51:35 +0530

    Update PostData.js
    url

--- src/services/PostData.js
+++ src/services/PostData.js
@@ -1,5 +1,5 @@
 export function PostData(type, userData) {
-  let BaseURL = "http://www.phpmyadmin.co/index.php";
+  let BaseURL = "https://github.com/parixitsoni/feedback-system/blob/master/src/services/config.php";
   // let temp = "https://github.com/parixitsoni/feedback-system/blob/master/src/Database/php/react-php/api/config.php/";
   // let temp1 = "https://github.com/parixitsoni/feedback-system/blob/master/src/Database/php/react-php/api/index.php";
   return new Promise((resolve, reject) => {
